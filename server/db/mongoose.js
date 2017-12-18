858aa6b4a8bf3fbd55c34552e9b678f8183ef1e2
yogendra saxena <yogendrasaxena56@gmail.com>
2017-12-19 04:10:12 +0530

    Adding Moment.JS

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,4 +1,9 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/TodoApp");
+mongoose.connect("mongodb://localhost:27017/TodoApp").then(() => {
+  console.log("CONNECTION SUCCESS");
+
+},(error) => {
+  console.log("ERROR "+error);
+});
